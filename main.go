25570fc4d4166246b2d477cc02ee5f03edce80a5
Valentin Nastenko <nastenko@figleaf.com>
2021-05-02 15:19:00 +0300

    fix

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ import (
 
 const (
 	defaultPath = "/usr/local/bin/terraform" //default bin installation dir
-	version     = "tfselect 0.1.6\n"
+	version     = "tfselect 0.1.7\n"
 )
 
 //var version string
