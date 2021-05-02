4d88844c641e61002d12c2157b44f5bdc9ece181
Valentin Nastenko <nastenko@figleaf.com>
2021-05-02 16:33:58 +0300

    fix brew

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 EXE  := tfselect
 PKG  := github.com/basti0nz/tfselect
-VER := 0.1.11
+VER := 0.1.12
 PATH := build:$(PATH)
 GOOS ?= $(shell go env GOOS)
 GOARCH ?= $(shell go env GOARCH)
--- main.go
+++ main.go
@@ -22,7 +22,7 @@ import (
 
 const (
 	defaultPath = "/usr/local/bin/terraform" //default bin installation dir
-	version     = "tfselect 0.1.11\n"
+	version     = "tfselect 0.1.12\n"
 )
 
 //var version string
